765126cd6b75de26604d5ea78af55b778da271c3
Leon Ji <mgleon08@gmail.com>
2019-05-12 00:31:50 +0800

    Print the clock

--- main.go
+++ main.go
@@ -2,15 +2,32 @@ package main
 
 import (
 	"fmt"
+	"time"
 
 	"github.com/mgleon08/led-clock/placeholder"
 )
 
 func main() {
-	for line := 0; line < len(placeholder.Digits[0]); line++ {
-		for digit := range placeholder.Digits {
-			fmt.Print(placeholder.Digits[digit][line], "  ")
+	for {
+		now := time.Now()
+		hour, min, sec := now.Hour(), now.Minute(), now.Second()
+
+		clock := [...][5]string{
+			placeholder.Digits[hour/10], placeholder.Digits[hour%10],
+			placeholder.Colon,
+			placeholder.Digits[min/10], placeholder.Digits[min%10],
+			placeholder.Colon,
+			placeholder.Digits[sec/10], placeholder.Digits[sec%10],
 		}
-		fmt.Println()
+
+		for line := range clock[0] {
+			for digit := range clock {
+				fmt.Print(clock[digit][line], "  ")
+			}
+			fmt.Println()
+		}
+
+		time.Sleep(1 * time.Second)
 	}
+
 }
